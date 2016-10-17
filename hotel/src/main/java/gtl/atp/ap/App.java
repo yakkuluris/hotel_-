19a7f3a69e4a695573ef2a1e0eda0e863bd69daa
suresh <sureshyakkuluri@gmail.com>
2016-10-17 03:20:33 -0400

    maven project added

---
+++ hotel/src/main/java/gtl/atp/ap/App.java
@@ -0,0 +1,21 @@
+package gtl.atp.ap;
+
+import org.joda.time.LocalTime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        LocalTime currentTime = new LocalTime();
+		System.out.println("The current local time is: " + currentTime);
+		System.out.println( "Hello World!" );
+    }
+}
+
+
+
+
